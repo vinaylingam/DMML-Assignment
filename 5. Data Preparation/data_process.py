74d4460e1842d09fb69495753fe3cf773383a659
vinay lingam <vinaylingam18@gmail.com>
2025-08-23 23:09:55 +0530

    Data Preparation 2025/08/23

--- 5. Data Preparation/data_process.py
+++ 5. Data Preparation/data_process.py
@@ -2,33 +2,39 @@ import os
 import pandas as pd
 from logger import Logger
 from datetime import datetime
-from validate_and_report import validate_and_report
+from clean_data import clean_data
 
 # Assuming Logger class + validate_and_report already defined above
 
-logger = Logger('4. Data Validation/data_validation.log')
+logger = Logger('5. Data Preparation/data_preparation.log')
 
 today = datetime.today().strftime("%Y\\%m\\%d")
 
 # HuggingFace CSV path & report
 huggingface_csv = fr"3. Raw Data Storage\HuggingFace\{today}\customer_churn_dataset.csv"
-huggingface_report = fr"4. Data Validation\HuggingFace\{today}"
+huggingface_report = fr"5. Data Preparation\HuggingFace\{today}"
 
 # Kaggle CSV path & report
 kaggle_csv = fr"3. Raw Data Storage\Kaggle\{today}\customer_churn_dataset.csv"
-kaggle_report = fr"4. Data Validation\Kaggle\{today}"
+kaggle_report = fr"5. Data Preparation\Kaggle\{today}"
 
 def data_validator(input_csv, report_dir):
     try:
-        logger.log(f"Pipeline started for {input_csv}")
+        logger.log("-------------------------------------------------------")
+        logger.log(f"data preparation Pipeline started for {input_csv}")
 
         # 1. Load CSV
         df = pd.read_csv(input_csv)
         logger.log(f"Loaded CSV with shape {df.shape}")
+        
+        # 2 clean
+        df_cleaned = clean_data(df, logger)
 
-        # 2. Generate validation report
-        report_path = os.path.join(report_dir, "validation_report.xlsx")
-        validate_and_report(df, report_path, logger)
+        # 3. Save cleaned CSV back (optional, only if you want to keep a cleaned copy)
+        os.makedirs(report_dir, exist_ok=True)
+        cleaned_path = os.path.join(report_dir, "cleaned_churn_dataset.csv")
+        df_cleaned.to_csv(cleaned_path, index=False)
+        logger.log(f"Cleaned CSV saved to {cleaned_path}")
 
         logger.log("Pipeline completed successfully")
 
---
+++ 5. Data Preparation/logger.py
@@ -0,0 +1,18 @@
+# logger.py
+import os
+from datetime import datetime
+
+class Logger:
+    def __init__(self, log_file="2. Data Ingestion/data_ingestion.log"):
+        """
+        Initialize Logger with a log file path.
+        Ensures directory exists.
+        """
+        self.log_file = log_file
+        os.makedirs(os.path.dirname(self.log_file), exist_ok=True)
+
+    def log(self, message: str):
+        """Append a log message with timestamp to the log file."""
+        with open(self.log_file, "a", encoding="utf-8") as log:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            log.write(f"[{timestamp}] {message}\n")
